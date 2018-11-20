bdece29fd135fd7af94191ab34ccaa07589ecd7f
javtocesp <javi.cesp@gmail.com>
2018-11-19 21:39:25 -0300

    Create basicontainer.go

---
+++ basicontainer.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+      "fmt"
+      "os"
+      "os/exec"
+      "syscall"
+)
+
+func main(){
+     switch os.Args[1] {
+     case "run":
+          run ()
+     default :
+          panic ("What the fuck...")
+     }
+}
+
+func run(){
+     fmt.Printf("running %v\n",os.Args[2:])
+     cmd:= exec.Command(os.Args[2],os.Args[3:]...)
+     cmd.Stdin=os.Stdin
+     cmd.Stderr=os.Stderr
+     cmd.Stdout=os.Stdout
+
+     cmd.SysProcAttr = &syscall.SysProcAttr{
+         Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+     }
+     must (cmd.Run())
+}
+
+func must (err error){
+     if err!=nil{
+        panic(err)
+     }
+}
